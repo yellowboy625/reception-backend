2e586a552556000659a54bfea93a00113a81d521
yellowboy625 <see.ephraim@gmail.com>
2021-03-05 08:54:47 +0800

    capital PORT

--- backend.js
+++ backend.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const parse = require('csv-parse/lib/sync');
 const fs = require('fs');
 const path = require('path');
